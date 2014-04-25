f47130160efa96e816c211ee086cc5534e86ea84
vitaesthetica <vitaesthetica@icloud.com>
2014-04-25 11:57:32 -0500

    NEw Approach

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-  <title>Vita Æsthetica</title>
+  <title>scenic_photo</title>
   <meta name="description" content="website description" />
   <meta name="keywords" content="website keywords, website keywords" />
   <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
@@ -70,11 +70,58 @@
         </div>
       </div>
       <div class="content">
-        <h1>Welcome to the scenic_photo template</h1>
-        <p>This simple, fixed width website template is released under a <a href="http://creativecommons.org/licenses/by/3.0">Creative Commons Attribution 3.0 Licence</a>. This means you are free to download and use it for personal and commercial projects. However, you <strong>must leave the 'design from css3templates.co.uk' link in the footer of the template</strong>.</p>
-        <p>This template is written entirely in <strong>HTML5</strong> and <strong>CSS3</strong>. The photo used in the design was taken by me.</p>
-        <p>You can view more free CSS3 web templates <a href="http://www.css3templates.co.uk">here</a>.</p>
-        <p>This template is a fully documented 5 page website, with an <a href="examples.html">examples</a> page that gives examples of all the styles available with this design. There is also a working PHP contact form on the contact page.</p>
+        <h1>Contact Us</h1>
+        <p>Say hello, using this contact form.</p>
+        <?php
+          // Set-up these 3 parameters
+          // 1. Enter the email address you would like the enquiry sent to
+          // 2. Enter the subject of the email you will receive, when someone contacts you
+          // 3. Enter the text that you would like the user to see once they submit the contact form
+          $to = 'enter email address here';
+          $subject = 'Enquiry from the website';
+          $contact_submitted = 'Your message has been sent.';
+
+          // Do not amend anything below here, unless you know PHP
+          function email_is_valid($email) {
+            return preg_match('/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i',$email);
+          }
+          if (!email_is_valid($to)) {
+            echo '<p style="color: red;">You must set-up a valid (to) email address before this contact page will work.</p>';
+          }
+          if (isset($_POST['contact_submitted'])) {
+            $return = "\r";
+            $youremail = trim(htmlspecialchars($_POST['your_email']));
+            $yourname = stripslashes(strip_tags($_POST['your_name']));
+            $yourmessage = stripslashes(strip_tags($_POST['your_message']));
+            $contact_name = "Name: ".$yourname;
+            $message_text = "Message: ".$yourmessage;
+            $user_answer = trim(htmlspecialchars($_POST['user_answer']));
+            $answer = trim(htmlspecialchars($_POST['answer']));
+            $message = $contact_name . $return . $message_text;
+            $headers = "From: ".$youremail;
+            if (email_is_valid($youremail) && !eregi("\r",$youremail) && !eregi("\n",$youremail) && $yourname != "" && $yourmessage != "" && substr(md5($user_answer),5,10) === $answer) {
+              mail($to,$subject,$message,$headers);
+              $yourname = '';
+              $youremail = '';
+              $yourmessage = '';
+              echo '<p style="color: blue;">'.$contact_submitted.'</p>';
+            }
+            else echo '<p style="color: red;">Please enter your name, a valid email address, your message and the answer to the simple maths question before sending your message.</p>';
+          }
+          $number_1 = rand(1, 9);
+          $number_2 = rand(1, 9);
+          $answer = substr(md5($number_1+$number_2),5,10);
+        ?>
+        <form id="contact" action="contact.php" method="post">
+          <div class="form_settings">
+            <p><span>Name</span><input class="contact" type="text" name="your_name" value="<?php echo $yourname; ?>" /></p>
+            <p><span>Email Address</span><input class="contact" type="text" name="your_email" value="<?php echo $youremail; ?>" /></p>
+            <p><span>Message</span><textarea class="contact textarea" rows="5" cols="50" name="your_message"><?php echo $yourmessage; ?></textarea></p>
+            <p style="line-height: 1.7em;">To help prevent spam, please enter the answer to this question:</p>
+            <p><span><?php echo $number_1; ?> + <?php echo $number_2; ?> = ?</span><input type="text" name="user_answer" /><input type="hidden" name="answer" value="<?php echo $answer; ?>" /></p>
+            <p style="padding-top: 15px"><span>&nbsp;</span><input class="submit" type="submit" name="contact_submitted" value="send" /></p>
+          </div>
+        </form>
       </div>
     </div>
     <footer>
